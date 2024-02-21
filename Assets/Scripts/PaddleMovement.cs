6072a204f4a29417438cb56695dc042f36acd6f5
SyPhase <pi2568gamerz@gmail.com>
2024-02-20 18:53:31 -0700

    Moved PaddleMovement to Scripts folder + minor reformatting

--- Assets/Scripts/PaddleMovement.cs
+++ Assets/Scripts/PaddleMovement.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour
 {
-    public GameObject Center;
-    public float movespeed = 2;
-    // Update is called once per frame
+    [SerializeField] Transform Center;
+    [SerializeField] float movespeed = 2;
+
+    bool isRightPaddle;
+
+    void Start()
+    {
+        // True: to the right of the center aka arrow keys, False: to the left of the center aka wasd
+        isRightPaddle = (Center.position.x < this.transform.position.x);
+    }
+
     void FixedUpdate()
     {
-       
-        if (Center.transform.position.x < this.transform.position.x)//to the right of the center aka arrow keys
+        if (isRightPaddle)
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
                 this.transform.position = new Vector3(transform.position.x , transform.position.y + movespeed);
-                
             }
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 this.transform.position = new Vector3(transform.position.x, transform.position.y - movespeed);
             }
         }
-        else if(Center.transform.position.x > this.transform.position.x)//to the left of the center aka wasd
+        else
         {
             if (Input.GetKey(KeyCode.W))
             {
@@ -32,8 +38,6 @@ public class PaddleMovement : MonoBehaviour
             {
                 this.transform.position = new Vector3(transform.position.x, transform.position.y - movespeed);
             }
-
         }
-        
     }
 }
